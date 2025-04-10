47180d97989107841f702dfeedd80d55a9a1d637
Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>
2025-04-10 12:00:31 +0200

    **Summary:** Functions depending on function-sdk-python 0.6.0 are giving a segmentation fault. When (re)running the tests for this module, the following is shown:
    ```
    runtime_version.py:98: UserWarning: Protobuf gencode version 5.29.0 is exactly one major version older than the runtime version 6.30.2 at crossplane/function/proto/v1beta1/run_function.proto. Please update the gencode to avoid compatibility violations in the next runtime release.
    ```
    
    Apparently when renovatebot opened a PR with a bump up on `protobuf` version,
    the major version change violated the constraint with `grpcio-tools`, which
    has the following dependency:
    
    ```
    Collecting protobuf<6.0dev,>=5.26.1 (from grpcio-tools==1.71.0)
    ```
    
    This makes incompatible to have different versions of protobuf for the `build`
    venv and the `generate` one.
    
    Signed-off-by: Jesús Fernández <7312236+fernandezcuesta@users.noreply.github.com>

--- crossplane/function/proto/v1/run_function_pb2_grpc.py
+++ crossplane/function/proto/v1/run_function_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 from crossplane.function.proto.v1 import run_function_pb2 as crossplane_dot_function_dot_proto_dot_v1_dot_run__function__pb2
 
-GRPC_GENERATED_VERSION = '1.67.0'
+GRPC_GENERATED_VERSION = '1.71.0'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
